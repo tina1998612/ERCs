4b74ecb8043905fe235a00bec6a81d974b4cfea1
李婷婷 Lee Ting Ting <tina1998612@users.noreply.github.com>
2024-01-06 23:31:41 -0800

    Update IOpmlLib.sol

--- assets/erc-7007/contracts/IOpmlLib.sol
+++ assets/erc-7007/contracts/IOpmlLib.sol
@@ -1,10 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
 interface IOpmlLib {
   function initOpmlRequest(bytes32 inputHash) external returns (uint256 requestId); // we can construct the initialState using the input, can replace input with prompt (string)
-	function uploadResult(uint256 requestId, bytes32 output);
-	function startChallenge(uint256 requestId, finalState bytes32) returns (uint256 challengeId);
+	function uploadResult(uint256 requestId, bytes32 output) external;
+	function startChallenge(uint256 requestId, bytes32 finalState) external returns (uint256 challengeId);
 	function respondState(uint256 challengeId, bytes32 stateHash) external;
 	function proposeState(uint256 challengeId, bytes32 stateHash) external;
-	function assertStateTransition(uint256 challengeId);
-  function isFinalized(uint256 requestId) external returns (bool);
+	function assertStateTransition(uint256 challengeId) external;
+    	function isFinalized(uint256 requestId) external returns (bool);
 	function getOutputHash(uint256 requestId) external returns (bytes32 output);
 }
