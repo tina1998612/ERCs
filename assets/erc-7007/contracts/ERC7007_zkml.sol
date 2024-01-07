5ddedc62c3d5cae96023e813837f5372e85b33f3
李婷婷 Lee Ting Ting <tina1998612@users.noreply.github.com>
2024-01-07 16:04:13 +0900

    Update and rename ERC7007.sol to ERC7007_zkml.sol

--- assets/erc-7007/contracts/ERC7007_zkml.sol
+++ assets/erc-7007/contracts/ERC7007_zkml.sol
@@ -9,7 +9,7 @@ import "./IVerifier.sol";
 /**
  * @dev Implementation of the {IERC7007} interface.
  */
-contract ERC7007 is ERC165, IERC7007, ERC721URIStorage {
+contract ERC7007_zkml is ERC165, IERC7007, ERC721URIStorage {
     address public immutable verifier;
 
     /**
