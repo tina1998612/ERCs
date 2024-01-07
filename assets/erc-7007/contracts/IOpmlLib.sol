c7eb53276507b61f66d2543b37772b34e0e3d0e6
李婷婷 Lee Ting Ting <tina1998612@users.noreply.github.com>
2024-01-06 23:32:00 -0800

    Update IOpmlLib.sol

--- assets/erc-7007/contracts/IOpmlLib.sol
+++ assets/erc-7007/contracts/IOpmlLib.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 interface IOpmlLib {
-  function initOpmlRequest(bytes32 inputHash) external returns (uint256 requestId); // we can construct the initialState using the input, can replace input with prompt (string)
+  	function initOpmlRequest(bytes32 inputHash) external returns (uint256 requestId); // we can construct the initialState using the input, can replace input with prompt (string)
 	function uploadResult(uint256 requestId, bytes32 output) external;
 	function startChallenge(uint256 requestId, bytes32 finalState) external returns (uint256 challengeId);
 	function respondState(uint256 challengeId, bytes32 stateHash) external;
